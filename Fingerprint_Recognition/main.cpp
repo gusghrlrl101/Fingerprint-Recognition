24540f70b1b9e5f535909f40140f86c892e934ec
lololalayoho <3chlwlsdn@hanmail.net>
2019-04-08 20:18:14 +0900

    안녕하세요

--- Fingerprint_Recognition/main.cpp
+++ Fingerprint_Recognition/main.cpp
@@ -6,8 +6,11 @@ using namespace std;
 using namespace cv;
 
 int main() {
-	cout << "hello world!" << endl;
+	Mat img;
+	img = imread("E:\\opencv\\Fingerprint\\Fingerprint-Recognition\\Fingerprint_Recognition\\image\\Team1\\2019_1_1_L_I_1.bmp");
 
+	imshow("first", img);
+	waitKey(0);
 
 	return 0;
 }
