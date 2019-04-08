af2732499c0cad9f4087b7bf221d5e30256ab121
GodHyum <38372691+gusghrlrl101@users.noreply.github.com>
2019-04-08 19:13:23 +0900

    Add files via upload

---
+++ Fingerprint_Recognition/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace std;
+using namespace cv;
+
+int main() {
+	cout << "hello world!" << endl;
+
+
+	return 0;
+}
