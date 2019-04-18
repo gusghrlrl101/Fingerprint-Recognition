09894a0ddec475ae9b66f2b3b6cf8c2e524a2091
junghyeokLim <41901175+junghyeokLim@users.noreply.github.com>
2019-04-18 23:48:55 +0900

    update segmentation
    segmentation

--- Fingerprint_Recognition/segmentation.hpp
+++ Fingerprint_Recognition/segmentation.hpp
@@ -10,21 +10,21 @@ using namespace cv;
 Mat segmentation(Mat &src, Mat &dst) {
 
 	src.convertTo(dst, CV_8UC1);
+	medianBlur(dst, dst, 7);
 
 	Mat mask = getStructuringElement(1, Size(3, 3), Point(1, 1));
-//	adaptiveThreshold(dst, dst, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 13, 3);
-
-	imshow("before dst", dst);
-	erode(dst, dst, mask);
+	adaptiveThreshold(dst, dst, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 15, 2);
+	imshow("dst", dst);
 
 	Mat seg;
-
 	morphologyEx(dst, seg, MORPH_OPEN, mask, Point(-1, -1), 12);
-	imshow("seg", seg);
-	imshow("dst", dst);
-	Mat seg_inv;
-	threshold(seg, seg_inv, 70, 255, THRESH_BINARY_INV);
 
+	GaussianBlur(dst, dst, Size(9, 9), 7);
+	adaptiveThreshold(dst, dst, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 9, 1);
+	imshow("gaussian", dst);
+
+	erode(dst, dst, mask, Point(-1, -1), 1);
+	imshow("erode", dst);
 
 	return seg;
 }
